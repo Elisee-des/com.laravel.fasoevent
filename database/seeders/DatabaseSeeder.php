f1c6d43b512f4bb543a3a750dfd09456b3742790
Elisee-dev <esabidani@gmail.com>
2024-03-16 20:45:11 +0000

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,8 +19,8 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        // $this->call([
-        //     RoleAndPermissionSeeder::class,
-        // ]);
+        $this->call([
+            UserSeeder::class,
+        ]);
     }
 }
